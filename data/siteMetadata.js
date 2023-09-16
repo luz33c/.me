3edde7c494005d577eadc093805e8f73d659108b
luz33c <33cluz@gmail.com>
2023-09-16 16:06:39 +0800

    feat: setting info

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,13 +1,13 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: '一个程序员的blog',
+  author: 'luz33c',
+  headerTitle: 'Luz',
+  description: '一个程序员的blog，关于js，rust，react，徒步，Les Mills，电影，美食',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteRepo: 'https://github.cm/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
   mastodon: 'https://mastodon.social/@mastodonuser',
--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -14,12 +14,15 @@ export default function AuthorLayout({ children, content }: Props) {
   return (
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="space-y-2 pb-8 pt-6 md:space-y-5">
+        {/* <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            About
+            About Me
           </h1>
-        </div>
+        </div> */}
         <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
+          <div className="prose max-w-none pb-8 pt-8 dark:prose-invert xl:col-span-2">
+            {children}
+          </div>
           <div className="flex flex-col items-center space-x-2 pt-8">
             {avatar && (
               <Image
@@ -40,9 +43,7 @@ export default function AuthorLayout({ children, content }: Props) {
               <SocialIcon kind="twitter" href={twitter} />
             </div>
           </div>
-          <div className="prose max-w-none pb-8 pt-8 dark:prose-invert xl:col-span-2">
-            {children}
-          </div>
+         
         </div>
       </div>
     </>
